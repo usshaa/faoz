d90a9affa0104b754eb76de66d7b1df724fa3174
usshaa <usshaa47@gmail.com>
2023-09-10 18:29:27 +0530

    Initial commit

---
+++ dhoni/B.py
@@ -0,0 +1,2 @@
+def pro(n1,n2):
+    print(n1*n2)
---
+++ usha.py
@@ -0,0 +1,9 @@
+def greet():
+    print("welcome")
+
+class Student:
+    def __init__(self,b):
+        self.a = b
+    def display(self):
+        print(self.a)
+
